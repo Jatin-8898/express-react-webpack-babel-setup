9f25ecac243179702deb5d9d9ba0d27ff2d93aff
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-08 23:26:49 +0530

    Added Babel dependencies

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 let  app = express();
 
-app.get('/',(req,res)=>res.send("HELLO"));
+app.get('/', (req,res) => res.send("HELLO FROM EXPRESS"));
 
-app.listen(3000);
+app.listen(3000,  () => console.log("Example app listening on port 3000!"));

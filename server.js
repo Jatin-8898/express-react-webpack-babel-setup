953d9d2b47dce8206b307d1590d44d4c8a86ea21
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-02-08 23:32:31 +0530

    Added static files to serve

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 let  app = express();
 
-app.get('/', (req,res) => res.send("HELLO FROM EXPRESS"));
+//app.get('/', (req,res) => res.send("HELLO FROM EXPRESS"));
+
+app.use(express.static('public'));
 
 app.listen(3000,  () => console.log("Example app listening on port 3000!"));
